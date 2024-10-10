3fb20b636d8edb772e5623a04b44a7b8ac975ab4
Soham Patil <sspatil6@ncsu.edu>
2024-10-10 19:51:36 -0400

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -3,7 +3,10 @@
 grep -rl "sample" . | \
 xargs grep -l "CSC510" | \
 xargs grep -o "CSC510" | \
-uniq -c | grep -E '^[[:space:]]*3|^[[:space:]]*[4-9]|^[[:space:]]*[1-9][0-9]' | \
-sort -r | \
-gawk '{print $2}' | \
+uniq -c | \
+grep -E '^[[:space:]]*3|^[[:space:]]*[4-9]|^[[:space:]]*[1-9][0-9]' | \
+gawk '{print $1, $2}' | \
+while read count file; do echo "$count $(ls -l ${file%:*} | gawk '{print $5}') ${file%:*}"; done | \
+sort -k1,1nr -k2,2nr | \
+gawk '{print $3}' | \
 sed 's/file/filtered/g'
