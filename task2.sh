2bf1ede5424bc647a768b7e93d3dd1b69c036d0d
Soham Patil <sspatil6@ncsu.edu>
2024-10-10 20:17:58 -0400

    Remove loop

--- task2.sh
+++ task2.sh
@@ -6,7 +6,7 @@ xargs grep -o "CSC510" | \
 uniq -c | \
 grep -E '^[[:space:]]*3|^[[:space:]]*[4-9]|^[[:space:]]*[1-9][0-9]' | \
 gawk '{print $1, $2}' | \
-while read count file; do echo "$count $(ls -l ${file%:*} | gawk '{print $5}') ${file%:*}"; done | \
+gawk '{ "ls -l " substr($2, 1, index($2, ":")-1) " | gawk \047{print $5}\047" | getline size; print $1, size, substr($2, 1, index($2, ":")-1) }' | \
 sort -k1,1nr -k2,2nr | \
 gawk '{print $3}' | \
 sed 's/file/filtered/g'
